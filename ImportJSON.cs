93a4cdca3e48bae86d3a6157138ee147ca7699c6
Ilian Kukov <iliankukov@gmail.com>
2023-03-29 21:20:13 +0300

    Add project files.

---
+++ ImportJSON.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonSerialization
+{
+    public class ImportJSON
+    {
+        public List<T> entities { get; set; }
+        public int Customer { get; set; }
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using System.Xml.Linq;
+
+namespace JsonSerialization
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string jsonFileContent = File.ReadAllText(@"C:\Users\ikuko001\Desktop\SSMS GERMANY\Klarna\test.json");
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            js.MaxJsonLength = int.MaxValue;
+
+            List<ImportJSON> importJSONList = js.Deserialize<List<ImportJSON>>(jsonFileContent);
+
+            foreach (ImportJSON jsn in importJSONList)
+            {
+                ////Output0Buffer.addRow();
+                //Output0Buffer.id = jsn.Entities_id;
+                //Output0Buffer.name = jsn.Entities_name;
+                //Output0Buffer.devisionid = jsn.Entities_devision_id;
+                //Output0Buffer.devisionname = jsn.Entities_devision_name;
+                //Output0Buffer.devisionselfuri = jsn.Entities_devision_selfuri;
+                //Output0Buffer.chatjaberid = jsn.Chat_jabberId;
+                //Output0Buffer.department = jsn.Department;
+                //Output0Buffer.email = jsn.Email;
+                //Output0Buffer.primarycontactaddress = jsn.PrimaryContactInfo_adress;
+                //Output0Buffer.primarycontactmediatype = jsn.PrimaryContactInfo_mediaType;
+                //Output0Buffer.primarycontacttype = jsn.PrimaryContactInfo_type;
+                //Output0Buffer.address = jsn.Addresses;
+                //Output0Buffer.state = jsn.State;
+                //Output0Buffer.title = jsn.Title;
+                //Output0Buffer.username = jsn.Username;
+                //Output0Buffer.version = jsn.Version;
+                //Output0Buffer.acdautoanswer = jsn.AcdAutoAnswer;
+                //Output0Buffer.selfuri = jsn.SelfUri;
+
+                Console.WriteLine("test test test");
+            }
+        }
+    }
+}
